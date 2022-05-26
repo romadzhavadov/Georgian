98f58de42c2b5639acf118b64ad2220d136a4980
romadzhavadov <r.dzhavadov12@gmail.com>
2022-05-26 17:45:23 +0300

    changes taste

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ btnMenu.addEventListener('click', function() {
     btnMenu.classList.toggle('header__btn-menu--open');
     nav.classList.toggle('header__nav--open');
 })
+
+const swiper = new Swiper('.swiper', {
+slidesPerView: "auto",
+spaceBetween: 5,
+loop: true,
+pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+},
+
+navigation: {
+    nextEl: '.swiper-button-prev',
+    prevEl: '.swiper-button-next',
+},
+
+});
