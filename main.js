ed56a364f121eadeca3afda00abdcf5ceb342f7e
romadzhavadov <r.dzhavadov12@gmail.com>
2022-06-24 00:06:35 +0300

    add footer and finish project

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ btnMenu.addEventListener('click', function() {
 
 const swiper = new Swiper('.swiper', {
 slidesPerView: "auto",
+centeredSlides: true,
 spaceBetween: 5,
 loop: true,
 pagination: {
@@ -21,3 +22,20 @@ navigation: {
 },
 
 });
+
+// const picturesSwiper = new picturesSwiper('.pictures__swiper', {
+//     slidesPerView: "auto",
+//     centeredSlides: true,
+//     loop: true,
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//   });
+
